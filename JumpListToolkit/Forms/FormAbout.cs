870f36aa286951695d68614b62562e50162ad6a7
Enner Pérez <ennerperez@gmail.com>
2017-12-26 00:59:44 -0400

    Initial source

---
+++ JumpListToolkit/Forms/FormAbout.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Toolkit.Forms
+{
+    public partial class FormAbout : Form
+    {
+        public FormAbout()
+        {
+            InitializeComponent();
+
+            Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            labelAbout.Text = string.Format(labelAbout.Text,
+                ApplicationInfo.Description,
+                ApplicationInfo.Version.ToString(),
+                ApplicationInfo.CopyrightHolder);
+
+            richTextBoxChangeLog.Text = System.Text.Encoding.UTF8.GetString(Properties.Resources.CHANGELOG);
+            richTextBoxLicense.Text = System.Text.Encoding.UTF8.GetString(Properties.Resources.LICENSE);
+
+            pictureBoxIcon.Image = Icon.ToBitmap();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabelWeb_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start((sender as LinkLabel).Text);
+        }
+    }
+}
---
+++ JumpListToolkit/Models/Link.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Taskbar;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.Models
+{
+    public class Link
+    {
+        public Link()
+        {
+        }
+
+        public Link(string path, string title) : this()
+        {
+            Path = path;
+            Title = title;
+        }
+
+        public Link(string path, string title, string icon, int index = 0) : this(path, title)
+        {
+            this.IconLocation = icon;
+            this.IconIndex = index;
+        }
+
+        #region Properties
+
+        //
+        // Summary:
+        //     Gets or sets the icon reference (location) of the link's icon.
+        public string IconLocation { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the icon reference (index) of the link's icon.
+        public int IconIndex { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the link's title
+        public string Title { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the link's description
+        public string Description { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the link's path
+        public string Path { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the object's arguments (passed to the command line).
+        public string Arguments { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the object's working directory.
+        public string WorkingDirectory { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the show command of the launched application.
+        public WindowShowCommand ShowCommand { get; set; }
+
+        public string Category { get; set; }
+
+        public Link Clone()
+        {
+            return new Link(this.Path, this.Title)
+            {
+                Arguments = this.Arguments,
+                Category = this.Category,
+                Description = this.Description,
+                IconIndex = this.IconIndex,
+                IconLocation = this.IconLocation,
+                ShowCommand = this.ShowCommand,
+                WorkingDirectory = this.WorkingDirectory
+            };
+        }
+
+        #endregion Properties
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
